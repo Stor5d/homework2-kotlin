c0187511410eee8c0cdbdd1b3a812c027c60bbee
stor5d <stor5d@mail.ru>
2022-04-02 01:30:13 +0300

    Rename rezult

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -7,17 +7,16 @@ fun main() {
     val discountStandardAmount = 100_00
     val discountPercentage = 5
     val discountPercentageMusicLover = 1
-    val range1 = 1000_00
-    val range2 = 10000_00
-    val rezult1: Int = if (amountPrev <= range1) {
+    val rezultNoMusicLover: Int = if (amountPrev <= 1000_00) {
         amountCurr
-    } else if (amountPrev > range1 && amountPrev <= range2) {
+    } else if (amountPrev > 1000_00 && amountPrev <= 10000_00) {
         if (amountCurr > discountStandardAmount) amountCurr - discountStandardAmount else 0
     } else {
         (amountCurr * (1 - (discountPercentage / 100.00))).toInt()
     }
-    val rezult2: Int = if (musicLover) (rezult1 * (1 - (discountPercentageMusicLover / 100.00))).toInt() else rezult1
-    val rubles: Int = rezult2 / 100
-    val pennies: Int = rezult2 % 100
+    val rezult: Int =
+        if (musicLover) (rezultNoMusicLover * (1 - (discountPercentageMusicLover / 100.00))).toInt() else rezultNoMusicLover
+    val rubles: Int = rezult / 100
+    val pennies: Int = rezult % 100
     println("Стоймость покупки с учётом скидки: $rubles руб. $pennies коп.")
 }
